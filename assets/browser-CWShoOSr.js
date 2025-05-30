09b9e616283a9a401afbb207dbc27522ef145606
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2025-05-30 22:06:03 +0100

    Updates

---
+++ assets/browser-CWShoOSr.js
@@ -0,0 +1,24 @@
+import { g as c } from "./index-l3uR-bYx.js";
+function f(t, i) {
+  for (var o = 0; o < i.length; o++) {
+    const e = i[o];
+    if (typeof e != "string" && !Array.isArray(e)) {
+      for (const r in e) if (r !== "default" && !(r in t)) {
+        const s = Object.getOwnPropertyDescriptor(e, r);
+        s && Object.defineProperty(t, r, s.get ? s : { enumerable: true, get: () => e[r] });
+      }
+    }
+  }
+  return Object.freeze(Object.defineProperty(t, Symbol.toStringTag, { value: "Module" }));
+}
+var n, u;
+function b() {
+  return u || (u = 1, n = function() {
+    throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object");
+  }), n;
+}
+var a = b();
+const w = c(a), p = f({ __proto__: null, default: w }, [a]);
+export {
+  p as b
+};
